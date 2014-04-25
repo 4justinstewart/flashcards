0a20a0a49e989a1644e39fdaa0c7abad262e69c8
Ron Gierlach <rongierlach@yahoo.com>
2014-04-25 14:31:24 -0500

    new stuff

--- flash_cards/app/controllers/index.rb
+++ flash_cards/app/controllers/index.rb
@@ -1,5 +1,32 @@
 enable :sessions
 
+before '/*' do 
+  current_user
+  redirect '/' unless session[:user_id]
+end
+
 get '/' do
   erb :index
+end
+
+post '/login' do
+  @user = User.authenticate(params[:email], params[:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/dashboard'
+  else
+    redirect '/'
+  end
+end
+
+post '/signup' do
+  if params[:user][:password] == params[:verify_password]
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/dashboard'
+    end
+  else
+    redirect '/'
+  end
 end
--- flash_cards/app/models/user.rb
+++ flash_cards/app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
   has_many :decks
   has_many :rounds
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    return @user if @user && @user.password == password
+    return nil
+  end
+
 end
