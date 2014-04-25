149da83cad5260843f43bfe1bb7c0b1c46e29e87
Paige Crum <paigecrum@gmail.com>
2014-04-25 15:39:28 -0500

    preliminary dashboard.erb and dashboard.rb

--- flash_cards/app/controllers/dashboard.rb
+++ flash_cards/app/controllers/dashboard.rb
@@ -1,7 +1,9 @@
 enable :sessions
 
 get '/dashboard' do #/decks
-
+	@decks = Deck.all
+	@user = User.find_by_email(params[:email])
+	# @rounds = 
 # LIST ALL DECKS erb :show_decks, give CRUD options and ability to access user stats
   erb :dashboard
 end
