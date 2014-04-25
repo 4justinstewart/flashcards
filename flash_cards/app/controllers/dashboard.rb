61a6b8bb329b09c011f8eb5bfcea587f7227cd16
Paige Crum <paigecrum@gmail.com>
2014-04-25 16:09:00 -0500

    dashboard.erb and dashboard.rb for review by group at after 4PM checkin

--- flash_cards/app/controllers/dashboard.rb
+++ flash_cards/app/controllers/dashboard.rb
@@ -1,8 +1,10 @@
 enable :sessions
 
 get '/dashboard' do #/decks
-	@decks = Deck.all
-	@user = User.find_by_email(params[:email])
+	# session
+
+	# @decks = Deck.all
+	# @user = User.find_by_email(params[:email])
 	# @rounds = 
 # LIST ALL DECKS erb :show_decks, give CRUD options and ability to access user stats
   erb :dashboard
