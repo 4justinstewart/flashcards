620f624ad7b72fdb3144844f7c0c161b387a2e86
Ron Gierlach <rongierlach@yahoo.com>
2014-04-25 18:16:26 -0500

    Resolved merge conflicts, working dashboard view

--- flash_cards/app/controllers/dashboard.rb
+++ flash_cards/app/controllers/dashboard.rb
@@ -1,11 +1,19 @@
 enable :sessions
 
+before '/dashboard' do 
+  # redirect '/' unless session[:user_id]
+  current_user 
+  unless @user 
+    redirect '/'
+  end
+end
+
 get '/dashboard' do #/decks
 	# session
 
-	# @decks = Deck.all
+	@decks = Deck.all
 	# @user = User.find_by_email(params[:email])
-	# @rounds = 
+	@rounds = @user.rounds
 # LIST ALL DECKS erb :show_decks, give CRUD options and ability to access user stats
   erb :dashboard
 end
