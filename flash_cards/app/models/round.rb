0dedf0bf6100b5269e1d7c163453a1775390f633
Rohan Mathur <rmathur101@gmail.com>
2014-04-26 14:08:26 -0500

    Feature to get deck stats, user deck stats (over all), user deck stats (by round)

--- flash_cards/app/models/round.rb
+++ flash_cards/app/models/round.rb
@@ -2,4 +2,27 @@ class Round < ActiveRecord::Base
   has_many :guesses
   belongs_to :user
   belongs_to :deck
+
+
+#Rohan - stats
+
+  def get_num_guesses
+  	self.guesses.count
+  end
+
+  def get_num_correct
+  	num_correct = 0
+  	self.guesses.each do |guess|
+  		if guess.correct == true
+  			num_correct += 1
+  		end
+  	end
+  	return num_correct
+  end
+
+  def get_percent_correct
+  	((self.get_num_correct.to_f/self.get_num_guesses.to_f) * 100).round(2)
+  end
+
+
 end
