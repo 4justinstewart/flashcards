2eb4e834126ed36e2d1006d8c775de3e8950be2c
Rohan Mathur <rmathur101@gmail.com>
2014-04-27 12:02:40 -0500

    Integrated statistics with the dashboard (bar graphs - yay)

--- flash_cards/app/controllers/dashboard.rb
+++ flash_cards/app/controllers/dashboard.rb
@@ -14,6 +14,15 @@ get '/dashboard' do #/decks
 	# @user = User.find_by_email(params[:email])
 	@rounds = @user.rounds
 # LIST ALL DECKS erb :show_decks, give CRUD options and ability to access user stats
+
+	#Stats Stuff
+	user = User.find(session[:user_id])
+	@user_stats_by_deck = get_deck_names(user.get_stat_decks)
+	@deck_stats_by_deck = create_decks_stat_hash
+
+
+
+
   erb :dashboard
 end
 
